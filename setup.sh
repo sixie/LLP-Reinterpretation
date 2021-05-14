67e61dd99bd69ff5ceb8683b3bf20f094f7e8f8a
cmorgoth <cristian.morgoth@gmail.com>
2021-05-14 10:20:05 -0700

    update cards

--- setup.sh
+++ setup.sh
@@ -12,8 +12,9 @@ fi
 top_dir=`git rev-parse --show-toplevel`
 echo ${top_dir}
 #my_pythia8=/storage/user/cpena/work/new_installation_reinterpret/LLP-Reinterpretation/MG5_aMC_v2_9_3/HEPTools/pythia8/
-my_pythia8=${top_dir}/MG5_aMC_v2_9_3/HEPTools/pythia8/
+my_pythia8=${top_dir}/MG5_aMC_v2_9_3/HEPTools/pythia8
 echo ${my_pythia8}
+export PATH=${PATH}:${my_pythia8}/bin
 export PYTHIA8DATA=${my_pythia8}/share/Pythia8/xmldoc
 echo $PYTHIA8DATA
 
