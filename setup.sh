f418a80c66c2a473491456b7da4a84e77350dbc3
cmorgoth <cristian.morgoth@gmail.com>
2021-05-18 13:04:01 -0700

    update

--- setup.sh
+++ setup.sh
@@ -11,10 +11,10 @@ fi
 #####################
 top_dir=`git rev-parse --show-toplevel`
 echo ${top_dir}
-#my_pythia8=/storage/user/cpena/work/new_installation_reinterpret/LLP-Reinterpretation/MG5_aMC_v2_9_3/HEPTools/pythia8/
 my_pythia8=${top_dir}/MG5_aMC_v2_9_3/HEPTools/pythia8
 echo ${my_pythia8}
-export PATH=${PATH}:${my_pythia8}/bin
+export PATH=${my_pythia8}/bin:${PATH}
 export PYTHIA8DATA=${my_pythia8}/share/Pythia8/xmldoc
 echo $PYTHIA8DATA
-
+#export dir=/storage/user/cpena/work/new_clean_for_llp/CMSSW_9_4_20/src/LLP-Reinterpretation/
+export LD_LIBRARY_PATH=${top_dir}/MG5_aMC_v2_9_3/HEPTools/boost/lib:${LD_LIBRARY_PATH}
